32cf919e3be5817bd1e92459e45037937cd3d45e
Mateus <mcostat@outlook.com>
2023-08-26 12:30:01 -0300

    refactor

--- src/CalculadoraArquivo.py
+++ src/CalculadoraArquivo.py
@@ -6,8 +6,8 @@ class CalculadoraArquivo(CalculadoraConsole):
     def main(self):
         super().main()
         
-        with open("output.txt", "a+") as file:
-            file.write(self.entrada + "\n" + str(self.resultado))
+        with open("output-python.txt", "a+") as file:
+            file.write(self.entrada + "\n" + str(self.resultado) + "\n")
         
         
 if __name__ == "__main__":
